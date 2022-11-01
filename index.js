d316c3402c09fbc226d3b8acfeb988e20d5e66e8
2011CS010223 <115726120+2011CS010223@users.noreply.github.com>
2022-11-01 22:28:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 //npm install express
-
 var express = require('express');
-
 var app = express();
-
 // set port, listen for requests
-
 const PORT = 8080;
 
 app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
 
-console.log(`Server is running on port ${PORT}.`);
+// Default Route
+app.get('/', function (req, res) {
+   res.send('<h1>2011CS010223</h1>');
+})
+
+
+// Connect MySQL Database
+
+var mysql = require('mysql');
+
+// Create Connection to MySQL
+
+var connection = mysql.createConnection({
+
+host:'demo-223.cmkxcz3si1yf.us-east-1.rds.amazonaws.com',
+
+user:'root',
+
+password:'mysql123',
+
+database:'mru'
 
 });
 
-// Default Route – http://localhost:8080
+app.get("/mysql/StudentDetails", function(req , res){
 
-app.get('/', function (req, res) {
+    connection.query("SELECT *FROM mru.Student", function (err, data) {
+    
+    if (err) return next(new AppError(err, 500));
+    
+        res.send(data)
+    });
+    
+    });
 
-res.send('<h1>Hello World</h1>');
-
-})
